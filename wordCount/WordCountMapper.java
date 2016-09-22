b744d1cb83dc12657b9ca4f09627b2bde4655a5a
danwangkm <danwangkm@gmail.com>
2016-09-21 21:42:38 -0400

    changed the code to get rid of buffer in Mapper

--- wordCount/WordCountMapper.java
+++ wordCount/WordCountMapper.java
@@ -4,35 +4,20 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 	
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		String line = value.toString();
 		
-		HashMap<String, Integer> result = new HashMap<String, Integer>();
-        result.put("hackathon", 0);
-        result.put("Dec", 0);
-        result.put("Chicago", 0);
-        result.put("Java", 0);
-		wordCount("hackathon", line, result);
-        wordCount("Dec", line, result);
-        wordCount("Chicago", line, result);
-        wordCount("Java", line, result);
-
-        for (Map.Entry<String, Integer> entry : result.entrySet()) {
-        	context.write(new Text(entry.getKey()), new IntWritable(entry.getValue()));
+		String[] words = {"hackathon", "Dec", "Chicago", "Java"};
+        for (int i=0; i<words.length; i++) {
+            int count = 0;
+            if (line.toLowerCase().indexOf(words[i].toLowerCase()) > -1) {
+                count = 1;
+            }
+            context.write(new Text(words[i]), new IntWritable(count));
         }
 	}
-
-	public static void wordCount(String word, String line, HashMap<String, Integer> result) {
-
-        if (line.toLowerCase().indexOf(word.toLowerCase()) > -1) {
-            result.put(word, result.get(word)+1);
-        }
-    }
 }
 
