cecf6f053657873c50b5a8239f2163b286caa82a
danwangkm <danwangkm@gmail.com>
2016-09-27 02:04:05 -0400

    fixed mismatch bug in PageRank.java

--- pageRank/PageRank.java
+++ pageRank/PageRank.java
@@ -1,5 +1,4 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -23,7 +22,7 @@ public class PageRank {
         job.setReducerClass(PageRankReducer.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
