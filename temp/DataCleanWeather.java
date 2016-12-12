c3783abf93cda00644a0c0613ab43233156d1aca
danwangkm <danwangkm@gmail.com>
2016-12-12 14:34:45 -0500

    data profiling and cleaning code

---
+++ temp/DataCleanWeather.java
@@ -0,0 +1,34 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class DataCleanWeather {
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 2) {
+            System.err.println("Usage: DataCleanWeather <input path> <output path>");
+            System.exit(-1);
+        }
+
+        String inputPath = args[0];
+        String outputPath = args[1];
+        Job job = new Job();
+        job.setJarByClass(DataCleanWeather.class);
+        job.setJobName("DataCleanWeather");
+
+        FileInputFormat.addInputPath(job, new Path(inputPath));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        job.setMapperClass(WeatherMapper.class);
+        job.setReducerClass(NormalReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.waitForCompletion(true);
+        System.exit(0);
+    }
+}
